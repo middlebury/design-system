6dbe6bd2a90f37767724f748ba18ddc24794fc11
Zeb Pykosz <zeb.apy@gmail.com>
2018-04-11 16:11:27 -0400

    start adding docs homepage info

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
+import Link from 'gatsby-link';
 
 class Home extends Component {
   render() {
-    const {title} = this.props.data.site.siteMetadata;
+    const {title, description, version} = this.props.data.site.siteMetadata;
     return (
       <div>
         <h1>{title}</h1>
+        <p className="lead">{description}</p>
+        <small>Currently v{version}</small>
+
+        <div className="row">
+          <div className="col-6">
+            <h2>
+              <Link to="/getting-started">Getting Started</Link>
+            </h2>
+            <p>How to install, use the framework, code style, and more.</p>
+          </div>
+          <div className="col-6">
+            <h2>
+              <Link to="/components">Components</Link>
+            </h2>
+            <p>
+              Browse components with code examples, guidelines, and live
+              previews.
+            </p>
+          </div>
+          <div className="col-6">
+            <h2>
+              <Link to="/components">Style</Link>
+            </h2>
+            <p>
+              Foundational elements like color, grid, icons, and typography.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
@@ -18,6 +47,8 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        description
+        version
       }
     }
   }
