601bcfd71c07b66fcac09c5ebed8a2062d0691af
Zeb Pykosz <zeb.apy@gmail.com>
2018-04-26 15:06:53 -0400

    add copy svg and download buttons to icon library

--- site/src/components/IconLibrary.js
+++ site/src/components/IconLibrary.js
@@ -1,20 +1,50 @@
 import React from 'react';
 
-const IconLibrary = ({icons}) => (
+import HoverContent from './HoverContent';
+import CopyButton from './CopyButton';
+import Button from './Button';
+
+const getIconUrl = name =>
+  `https://unpkg.com/middlebury-design-system/dist/icons/svg/${name}.svg`;
+
+// TODO: serve icons from same host so download works
+const IconLibrary = ({ icons }) => (
   <div className="docs-icons">
-    {icons.edges
-      .sort((a, b) => (a.node.name < b.node.name ? -1 : 1))
-      .map(({node}) => (
-        <div key={node.id} className="docs-icons__item">
-          <div>
-            <span
-              key={node.id}
-              dangerouslySetInnerHTML={{__html: node.internal.content}}
-            />
-            <p>{node.name}</p>
-          </div>
-        </div>
-      ))}
+    <ul className="docs-icons__list">
+      {icons.edges
+        .sort((a, b) => (a.node.name < b.node.name ? -1 : 1))
+        .map(({ node }) => {
+          const svg = node.internal.content;
+
+          const content = (
+            <div className="docs-icons__actions">
+              <CopyButton label="Copy SVG" value={svg} className="mb-1" />
+              <Button
+                is="a"
+                href={getIconUrl(node.name)}
+                download={`${name}.svg`}
+                kind="light"
+                outline
+              >
+                Download
+              </Button>
+            </div>
+          );
+
+          return (
+            <li key={node.id} className="docs-icons__item">
+              <HoverContent
+                content={content}
+                className="docs-icons__card"
+                tabIndex={0}
+              >
+                <span key={node.id} dangerouslySetInnerHTML={{ __html: svg }} />
+              </HoverContent>
+              <p>{node.name}</p>
+            </li>
+          );
+        })}
+    </ul>
   </div>
 );
 
